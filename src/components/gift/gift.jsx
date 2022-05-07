e17ae3cee1f75bf1759737ab30ef38db27adf0af
RPapin <remi.papin@viacesi.fr>
2022-05-07 16:14:37 +0200

    remove bootstrap use js librairie for gift modal

--- src/components/gift/gift.jsx
+++ src/components/gift/gift.jsx
@@ -2,78 +2,87 @@ import React, { useState, useEffect } from 'react'
 import './gift.scss'
 
 
+
 const Gift = () => {
     const lastar_keycode = [76, 65, 83, 84, 65]
     const [show, setShow] = useState(false)
     const [keyPressed, setKeyPressed] = useState([])
 
+    const handleClose = () => setShow(false);
     useEffect( () => {
         
         
         console.log("useEffect");
         function handleKeyDown(e) {
-            console.log(keyPressed.toString());
-            console.log(lastar_keycode.toString());
-
+            
+            console.log(keyPressed);
             if(keyPressed.toString() === lastar_keycode.toString()){
                 console.log("SURPRISE");
                 setShow(true)
+                document.getElementById("btnToggleModal").click();
             }
             if(lastar_keycode.includes(e.keyCode)){
-                setKeyPressed(keyPressed.push(e.keyCode))
-            } else setKeyPressed([])
+                let tempKeyPressed = keyPressed
+                tempKeyPressed.push(e.keyCode)
+                setKeyPressed(tempKeyPressed)
+            } else{
+                setKeyPressed([]);
+            } 
           }
         document.addEventListener("keydown", handleKeyDown);
          // Don't forget to clean up
         return function cleanup() {
             document.removeEventListener('keydown', handleKeyDown);
         }
-    }, [show])
+    }, [keyPressed, show])
     return (
         <>
-            {show &&
+        <button type="button" class="btn btn-primary hide" data-toggle="modal" data-target="#myModal" id="btnToggleModal">Large modal</button>
+        <div className="modal fade" tabIndex="-1" role="dialog" id="myModal" aria-labelledby="myModalLabel">
+            <div className="modal-dialog" role="document">
+            <div className="modal-content">
                 <div className={'gift-container'} >
-                    <div class="card">
-                        <div class="back"></div>
-                        <div class="front">
-                            <div class="cover-shape-large">
-                            <div class="shape-diamond"></div>
-                            <div class="shape-block"></div>
+                    <div className="card">
+                        <div className="back"></div>
+                        <div className="front">
+                            <div className="cover-shape-large">
+                            <div className="shape-diamond"></div>
+                            <div className="shape-block"></div>
                             </div>
-                            <div class="cover-shape-small">
-                            <div class="shape-diamond"></div>
-                            <div class="shape-block">
-                                <div class="cake">
-                                <div class="layer layer-bottom"></div>
-                                <div class="layer layer-middle"></div>
-                                <div class="layer layer-top"></div>
-                                <div class="icing"></div>
-                                <div class="drip drip1"></div>
-                                <div class="drip drip2"></div>
-                                <div class="drip drip3"></div>
-                                <div class="candle">
-                                    <div class="flame"></div>
+                            <div className="cover-shape-small">
+                            <div className="shape-diamond"></div>
+                            <div className="shape-block">
+                                <div className="cake">
+                                <div className="layer layer-bottom"></div>
+                                <div className="layer layer-middle"></div>
+                                <div className="layer layer-top"></div>
+                                <div className="icing"></div>
+                                <div className="drip drip1"></div>
+                                <div className="drip drip2"></div>
+                                <div className="drip drip3"></div>
+                                <div className="candle">
+                                    <div className="flame"></div>
                                 </div>
                                 </div>
                             </div>
                             </div>
                         </div>
                         
-                        <div class="text-container">
+                        <div className="text-container">
                             <p id="head">Joyeux anniversaire ma star !</p>
-                            <p>Je t'offre une magnifique journée tous les deux avec au programme
-                                <ul>
-                                    <li>Restaurant</li>
-                                    <li>Shopping</li>
-                                    <li>Cinéma</li>
-                                </ul>
+                            <p>Je t'offre une magnifique journée tous les deux avec au programme:<br/>
+                            - Restaurant <br/>
+                            - Shopping <br/>
+                            - Cinéma 
                             </p>
 
                             <p>Trop hate !!!</p>
                         </div>
-                        </div>
+                    </div>
                 </div>
-            }
+            </div>
+            </div>
+        </div>
         </>
     )
 
